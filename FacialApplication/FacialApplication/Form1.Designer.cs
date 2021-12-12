e9c3613560ff095e99ff277848eed5dcdd2a251e
Jayson Levario <jaysonlevario13@aol.com>
2021-12-12 15:01:56 -0500

    committing to repository to check that Git is working

--- FacialApplication/FacialApplication/Form1.Designer.cs
+++ FacialApplication/FacialApplication/Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace FacialApplication
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(237, 90);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Run Python Script";
+            this.button1.Text = "Git Test";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
